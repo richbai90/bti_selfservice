9ce04448a650801ed0f9d3ff8657d9fad10d27cd
Administrator@SW.TEST <Administrator@SW.TEST>
2017-12-21 15:12:52 -0800

    Do not attempt to send a message to the log if we dont have a session first

---
+++ AdfsIntegration/html/_selfservice/selfservice/app/services/wss.logging.service.js
@@ -0,0 +1,93 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  angular.module('swSelfService').factory('wssLogging', wssLogging);
+
+  wssLogging.$inject = ['$q', 'XMLMCService', 'toaster', '$cookies'];
+
+  function wssLogging($q, XMLMCService, toaster, $cookies) {
+    var self = {};
+
+    //Log File Entry
+    self.logger = function (logDesc, logType, logSource, boolConsole, boolToast, toastTitle) {
+      var deferred = $q.defer();
+      var logDescription = '';
+      if (angular.isArray(logDesc) || angular.isObject(logDesc)) {
+        angular.forEach(logDesc, function (logVal, logKey) {
+          if (logDescription !== '') {
+            logDescription += ', ';
+          }
+          logDescription += '[' + logKey + '] ' + logVal;
+        });
+      } else {
+        logDescription = logDesc;
+      }
+
+      //Output to console
+      if (boolConsole === true) {
+        var objLog = {
+          logSource: logSource,
+          logType: logType,
+          logContent: logDesc
+        };
+        console.log(objLog);
+      }
+
+      if (boolToast === true) {
+        var toastType = '';
+        switch (logType) {
+          case 'WARN':
+            toastType = 'warning';
+            break;
+          case 'ERROR':
+            toastType = 'error';
+            break;
+          default:
+            toastType = 'info';
+        }
+        self.sendToast(toastType, logDescription, toastTitle);
+      }
+
+	  if($cookies.get('ESPSessionState')) {
+      var xmlmc = new XMLMCService.MethodCall();
+      var sqparams = "logclass=SYSTM";
+      sqparams += "&logtype=" + logType;
+      sqparams += "&logdesc=" + logDescription;
+      sqparams += "&logsource=" + logSource;
+      xmlmc.addParam("storedQuery", "query/wss/admin/admin.logging.insert");
+      xmlmc.addParam("parameters", sqparams);
+      xmlmc.invoke("data", "invokeStoredQuery", {
+        onSuccess: function onSuccess(params) {
+          deferred.resolve(params);
+        },
+        onFailure: function onFailure(error) {
+          console.log(error);
+          deferred.reject(error);
+        }
+      });
+      return deferred.promise;
+	  } else {
+		  console.log(logType, logDescription, logSource);
+		  console.trace();
+		  return $q.reject('No Session!!');
+	  }
+    };
+
+    self.sendToast = function (toastType, toastBody, toastTitle) {
+      var toastObject = {
+        type: toastType,
+        body: toastBody
+      };
+
+      if (angular.isDefined(toastTitle) && toastTitle !== '') {
+        toastObject.title = toastTitle;
+      }
+      toaster.pop(toastObject);
+    };
+
+    return self;
+  }
+})();
+//# sourceMappingURL=wss.logging.service.js.map
